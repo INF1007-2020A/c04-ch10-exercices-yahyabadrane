9cbbaeb4ef93aa30634348a41888e60cbf5be24a
yahyabadrane <yahyabadrane01@gmail.com>
2020-11-09 23:44:24 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -8,7 +8,7 @@ import numpy as np
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
 def linear_values() -> np.ndarray:
-    return np.array([])
+    return np.lins
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
